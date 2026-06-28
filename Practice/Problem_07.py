b1a228f0e7a3bc978de8619ca555255341a2079b
Md Ahtesham <md.ahtesham.feaid@gmail.com>
2026-06-28 22:02:25 +0530

    initial

---
+++ Practice/Problem_07.py
@@ -0,0 +1,5 @@
+#Write a program to wipe out the content of a file using Python.
+
+with open("wipe.txt", "w") as f:
+    f.write("")  #If you write anything inside the "" it will write the the wipe.txt so use "" so the if any content will be there it will wipe all those.
+    
