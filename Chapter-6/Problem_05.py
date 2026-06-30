9ab1c8009476b17bffae6e1912e691468902ad4c
Md Ahtesham <md.ahtesham.feaid@gmail.com>
2026-06-30 23:53:25 +0530

    init

---
+++ Chapter-6/Problem_05.py
@@ -0,0 +1,7 @@
+#Sara is pushing a crate on a smooth floor. The crate has a mass of 50 kg, and she applies a constant force of 150 N. Assuming no friction, calculate the acceleration of the crate using Newton’s Second Law of Motion.
+
+m = 50
+f = 150
+# We know that F = m*a.
+a = f / m
+print("The acceleration is:", a)
